662c9876b7e5a89d868fd5cf375ecd1349f1905c
Anusha-Mekala <160693437+Anusha-Mekala@users.noreply.github.com>
2024-02-21 15:41:39 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,6 +1,6 @@
 public class First {
     public static void main(String[] args){
-        System.out.println("Welcome to java programing");
+        System.out.println("Welcome to java programming");
     }
     
 }
